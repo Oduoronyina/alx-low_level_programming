803ba560781c08a9e42bb4ba149482afb8c43d3b
Oduoronyina <oduoronyina@outlook.com>
2022-04-23 20:20:43 +0300

    7. Mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * leet - a function that encodes a string into 1337
  *
@@ -15,13 +16,13 @@ int n[] = {52, 51, 48, 55, 49};
 /*iterate values in array s*/
 while (s[c] != '\0')
 {
-/
+/*
  * loop through array value five times
  * to check if the value is equal to
  * any small letter in array sl or
  * upper letter in array ul and if it
  * is replace it with the value in array n
- */
+*/
 for (i = 0; i < 5; i++)
 {
 if (s[c] == sl[i] || s[c] == ul[i])
