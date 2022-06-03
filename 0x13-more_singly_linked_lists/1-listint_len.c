6220d47cd19438807ed4db5e4415d9c37e20ea0f
Oduoronyina <oduoronyina@outlook.com>
2022-06-03 21:43:56 +0300

    a function that adds a new node at the end of a listint_t list.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,15 +7,15 @@
  *     */
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+size_t nodes = 0;
 
-	if (!h)
-		return (0);
-	while (h)
-	{
-		nodes += 1;
-		h = h->next;
-	}
-	return (nodes);
+if (!h)
+return (0);
+while (h)
+{
+nodes += 1;
+h = h->next;
+}
+return (nodes);
 }
 
